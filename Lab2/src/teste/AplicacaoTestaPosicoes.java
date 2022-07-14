eeffdba19d6630a4101ad8ee6bd204ddd450fefe
Matheus Costa <matheus.costa@inf.ufrgs.br>
2022-07-14 13:20:45 -0300

    get x y

--- Lab2/src/teste/AplicacaoTestaPosicoes.java
+++ Lab2/src/teste/AplicacaoTestaPosicoes.java
@@ -9,20 +9,31 @@ public class AplicacaoTestaPosicoes {
   int MAP_HEIGHT = 4;
 
   public static void main(String[] args) {
-    boolean vX, vY;
+    boolean vX = false, vY = false;
 
     Scanner scanner = new Scanner(System.in);
-    do {
-      System.out.println("Digite o valor de x: ");
+    System.out.println("Digite o valor de x(0 a 3): ");
 
-      int xInput = scanner.nextInt();
-      System.out.println("Digite o valor de y: ");
-      int yInput = scanner.nextInt();
+    int xInput = scanner.nextInt();
+    System.out.println("Digite o valor de y(0 a 3): ");
+    int yInput = scanner.nextInt();
 
-      PosicaoMapa4x4 p1 = new PosicaoMapa4x4(xInput, yInput);
+    PosicaoMapa4x4 p1 = new PosicaoMapa4x4(xInput, yInput);
+    vX = p1.setX(xInput);
+    vY = p1.setY(yInput);
+    // loop para verificar se ainda e falso
+    while (vX == false || vY == false) {
+      System.out.println("Digite um valor de x valido: ");
+
+      xInput = scanner.nextInt();
+      System.out.println("Digite um valor de y valido: ");
+
+      yInput = scanner.nextInt();
+
       vX = p1.setX(xInput);
       vY = p1.setY(yInput);
-    } while (!vX && !vY);
+    }
+p1.imprime();
   }
 
   /** private static void imprimeMapa(ArrayList<PosicaoMapa4x4> p) { } */
